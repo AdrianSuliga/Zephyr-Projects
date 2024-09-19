d4d35200c035a437ed23a5e7d1ceced08abf48d4
Adrian Suliga <adriansuliga81@gmail.com>
2024-09-19 15:35:27 +0200

    Change Blinky_PWM project to use timer and interrupts to control PWM pulse

--- Blinky_PWM/src/main.c
+++ Blinky_PWM/src/main.c
@@ -1,7 +1,35 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/pwm.h>
 
+#define SLEEPTIME 25
+
+static uint32_t ret = 0;
+static uint32_t pulse = 0;
+static bool is_increasing = true;
+
 static const struct pwm_dt_spec fading_led = PWM_DT_SPEC_GET(DT_NODELABEL(fading_led));
+
+static void delta_timer_handler(struct k_timer *timer_info)
+{
+    if (is_increasing) {
+        ret = pwm_set_pulse_dt(&fading_led, pulse);
+        pulse += 100;
+        if (pulse == 7500)
+            is_increasing = false;
+    } else {
+        ret = pwm_set_pulse_dt(&fading_led, pulse);
+        pulse -= 100;
+        if (pulse == 0)
+            is_increasing = true;
+    }
+
+    if (ret != 0) {
+        printk("PWM ERROR\n");
+        return;
+    }
+}
+
+K_TIMER_DEFINE(delta_timer, delta_timer_handler, NULL);
 
 int main(void)
 {
@@ -9,20 +37,10 @@ int main(void)
         return -1;
     }
 
-    int ret = 0;
-    int pulse = 0;
+    k_timer_start(&delta_timer, K_MSEC(SLEEPTIME), K_MSEC(SLEEPTIME));
 
     while (1) {
-        while (pulse < 10000) {
-            pwm_set_pulse_dt(&fading_led, pulse);
-            pulse += 10;
-            k_msleep(1);
-        }
-        while (pulse > 0) {
-            pwm_set_pulse_dt(&fading_led, pulse);
-            pulse -= 10;
-            k_msleep(1);
-        }
+
     }
     
     return 0;
